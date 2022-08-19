53baaa0fcfe480bab014bea08421a5eee12e4ec3
lby <liubinyi@zuoyebang.com>
2022-08-19 20:24:28 +0800

    新增节点 支持 输入输出

--- res/imguix/menu/menu_all.lua
+++ res/imguix/menu/menu_all.lua
@@ -96,3 +96,24 @@ end)
 
 
 
+
+
+
+
+
+
+
+
+
+
+-- global for imgui
+ImGui.HelpMarker = function(desc)
+    ImGui.TextDisabled("(?)");
+    if ImGui.IsItemHovered() then
+        ImGui.BeginTooltip();
+        ImGui.PushTextWrapPos(ImGui.GetFontSize() * 35.0);
+        ImGui.TextUnformatted(desc);
+        ImGui.PopTextWrapPos();
+        ImGui.EndTooltip();
+    end
+end
--- res/imguix/popup/add_dnode.lua
+++ res/imguix/popup/add_dnode.lua
@@ -13,14 +13,20 @@ function _p:ctor()
     self.sType_ = ""
 
     self.is_show = false
+    self.input_list = {}
+    self.output_list = {}
 end
 
 function _p:show()
     self.is_show = true
+    self.input_list = {}
+    self.output_list = {}
 end
 
 function _p:hide()
     self.is_show = false
+    self.input_list = {}
+    self.output_list = {}
 end
 
 function _p:isShow()
@@ -66,6 +72,196 @@ function _p:render()
                 end
                 ImGui.EndCombo()
             end
+            ImGui.Text("输入: ")
+            ImGui.SameLine()
+            ImGui.HelpMarker("新增输入参数：\n inputKey: 输入对象的查找索引\n direct: 输入参数在节点中的摆放位置\n key: 输入的类型\n numMax: 可拖拽的节点最大拖拽数量\n desc: 描述信息")
+            ImGui.SameLine()
+            if (ImGui.Button("+##input")) then
+                --ViewManager:showTip("新增")
+                table.insert(self.input_list, {
+                    inputKey = "",
+                    direct = enum.node_direct.left,
+                    key = enum.dropnode_key.input_text,
+                    desc = "",
+                    numMax = 0,
+                })
+            end
+            if self.input_list and #self.input_list > 0 then
+                for i, v in ipairs(self.input_list) do
+                    local curKey = i
+                    local curValue = v
+                    if (ImGui.Button("-" .. "##" .. "addinput" .. curKey)) then
+                        --ViewManager:showTip("" .. curKey)
+                        table.remove(self.input_list, curKey)
+                    end
+                    ImGui.SameLine()
+                    ImGui.Text("inputKey: ")
+                    ImGui.SameLine()
+                    ImGui.SetNextItemWidth(200)
+                    curValue.inputKey = ImGui.InputText("" .. "##input_" .. "inputKey" .. curKey, curValue.inputKey or "", 100);
+
+                    ImGui.SameLine()
+                    ImGui.Text("      direct: ")
+                    ImGui.SameLine()
+                    --curValue.direct = ImGui.InputText("" .. "##input_" .. "direct" .. curKey, curValue.direct or "", 100);
+                    ImGui.SetNextItemWidth(100)
+                    local is_sel_direct = false
+                    local sel_items_direct = table.values(enum.node_direct)
+                    if (ImGui.BeginCombo("" .. "##input_" .. "direct" .. curKey, curValue.direct or enum.node_direct.left)) then
+                        for i, v in ipairs(sel_items_direct) do
+                            is_sel_direct = false
+                            if curValue.direct == v then
+                                is_sel_direct = true
+                            end
+                            if ImGui.Selectable(v, is_sel_direct) then
+                                curValue.direct = v
+                            end
+                            if is_sel_direct then
+                                ImGui.SetItemDefaultFocus()
+                            end
+                        end
+                        ImGui.EndCombo()
+                    end
+
+
+                    ImGui.Text("      inputType: ")
+                    ImGui.SameLine()
+                    ImGui.SetNextItemWidth(200)
+                    --curValue.key = ImGui.InputText("" .. "##input_" .. "inputType" .. curKey, curValue.key or "", 100);
+                    local is_sel_inputType = false
+                    local sel_items_inputType_all = table.values(enum.dropnode_key)
+                    local sel_items_inputType = {}
+                    for i, v in ipairs(sel_items_inputType_all) do
+                        if string.find(v, "input_") or string.find(v, "in_") then
+                            table.insert(sel_items_inputType, v)
+                        end
+                    end
+                    if (ImGui.BeginCombo("" .. "##input_" .. "inputType" .. curKey, curValue.key or enum.dropnode_key.input_text)) then
+                        for i, v in ipairs(sel_items_inputType) do
+                            is_sel_inputType = false
+                            if curValue.key == v then
+                                is_sel_inputType = true
+                            end
+                            if ImGui.Selectable(v, is_sel_inputType) then
+                                curValue.key = v
+                            end
+                            if is_sel_inputType then
+                                ImGui.SetItemDefaultFocus()
+                            end
+                        end
+                        ImGui.EndCombo()
+                    end
+
+                    ImGui.SameLine()
+                    ImGui.Text("      numMax: ")
+                    ImGui.SameLine()
+                    ImGui.SetNextItemWidth(140)
+                    curValue.numMax = ImGui.InputInt("" .. "##input_" .. "numMax" .. curKey, curValue.numMax or 0);
+
+                    ImGui.Text("      desc: ")
+                    ImGui.SameLine()
+                    curValue.desc = ImGui.InputText("" .. "##input_" .. "desc" .. curKey, curValue.desc or "", 100);
+
+                end
+            end
+
+            ImGui.Separator()
+
+
+            ImGui.Text("输出: ")
+            ImGui.SameLine()
+            ImGui.HelpMarker("新增输出参数：\n outputKey: 输入对象的查找索引\n direct: 输入参数在节点中的摆放位置\n key: 输入的类型\n numMax: 可拖拽的节点最大拖拽数量\n desc: 描述信息")
+            ImGui.SameLine()
+            if (ImGui.Button("+##output")) then
+                --ViewManager:showTip("新增")
+                table.insert(self.output_list, {
+                    outputKey = "",
+                    direct = enum.node_direct.right,
+                    key = enum.dropnode_key.input_text,
+                    desc = "",
+                    numMax = 0,
+                })
+            end
+            if self.output_list and #self.output_list > 0 then
+                for i, v in ipairs(self.output_list) do
+                    local curKey = i
+                    local curValue = v
+                    if (ImGui.Button("-" .. "##" .. "addoutput" .. curKey)) then
+                        --ViewManager:showTip("" .. curKey)
+                        table.remove(self.output_list, curKey)
+                    end
+                    ImGui.SameLine()
+                    ImGui.Text("outputKey: ")
+                    ImGui.SameLine()
+                    ImGui.SetNextItemWidth(200)
+                    curValue.outputKey = ImGui.InputText("" .. "##output_" .. "outputKey" .. curKey, curValue.outputKey or "", 100);
+
+                    ImGui.SameLine()
+                    ImGui.Text("      direct: ")
+                    ImGui.SameLine()
+                    --curValue.direct = ImGui.InputText("" .. "##output_" .. "direct" .. curKey, curValue.direct or "", 100);
+                    ImGui.SetNextItemWidth(100)
+                    local is_sel_direct = false
+                    local sel_items_direct = table.values(enum.node_direct)
+                    if (ImGui.BeginCombo("" .. "##output_" .. "direct" .. curKey, curValue.direct or enum.node_direct.right)) then
+                        for i, v in ipairs(sel_items_direct) do
+                            is_sel_direct = false
+                            if curValue.direct == v then
+                                is_sel_direct = true
+                            end
+                            if ImGui.Selectable(v, is_sel_direct) then
+                                curValue.direct = v
+                            end
+                            if is_sel_direct then
+                                ImGui.SetItemDefaultFocus()
+                            end
+                        end
+                        ImGui.EndCombo()
+                    end
+
+
+                    ImGui.Text("      outputType: ")
+                    ImGui.SameLine()
+                    ImGui.SetNextItemWidth(200)
+                    --curValue.key = ImGui.InputText("" .. "##output_" .. "inputType" .. curKey, curValue.key or "", 100);
+                    local is_sel_inputType = false
+                    local sel_items_inputType_all = table.values(enum.dropnode_key)
+                    local sel_items_inputType = {}
+                    for i, v in ipairs(sel_items_inputType_all) do
+                        if string.find(v, "out_") then
+                            table.insert(sel_items_inputType, v)
+                        end
+                    end
+                    if (ImGui.BeginCombo("" .. "##output_" .. "outputType" .. curKey, curValue.key or enum.dropnode_key.input_text)) then
+                        for i, v in ipairs(sel_items_inputType) do
+                            is_sel_inputType = false
+                            if curValue.key == v then
+                                is_sel_inputType = true
+                            end
+                            if ImGui.Selectable(v, is_sel_inputType) then
+                                curValue.key = v
+                            end
+                            if is_sel_inputType then
+                                ImGui.SetItemDefaultFocus()
+                            end
+                        end
+                        ImGui.EndCombo()
+                    end
+
+                    ImGui.SameLine()
+                    ImGui.Text("      numMax: ")
+                    ImGui.SameLine()
+                    ImGui.SetNextItemWidth(140)
+                    curValue.numMax = ImGui.InputInt("" .. "##output_" .. "numMax" .. curKey, curValue.numMax or 0);
+
+                    ImGui.Text("      desc: ")
+                    ImGui.SameLine()
+                    curValue.desc = ImGui.InputText("" .. "##output_" .. "desc" .. curKey, curValue.desc or "", 100);
+
+                end
+            end
+
+            ImGui.Separator()
 
             ImGui.Text("项目名称(default:common)：")
 
