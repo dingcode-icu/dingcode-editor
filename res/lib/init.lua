ee1d7fcce5ad55a4fe905e89263cbc0b40a89759
dwb <dwb@dwb.ren>
2022-10-08 14:36:11 +0800

    fix:shadermgr 管理器

--- res/lib/init.lua
+++ res/lib/init.lua
@@ -5,16 +5,16 @@ require("lib/display_ex")
 require("lib/extends/node_ex")
 require("lib/extends/sprite_ex")
 
--- local SHADER_RES = {
---     outline = {"assets/shader/default.vert", "assets/shader/sp_outline.frag", {
---         u_outlineColor = {
---             x = 153,
---             y = 0,
---             z = 51
---         },
---         u_threshold = 1,
---         u_radius = 0.01
---     }}
--- }
--- _G.ShaderMgr = require("lib/inc/shader_inc")
--- ShaderMgr:getInc():init(SHADER_RES)
+local SHADER_RES = {
+    outline = {"assets/shader/default.vert", "assets/shader/sp_outline.frag", {
+        u_outlineColor = {
+            x = 153,
+            y = 0,
+            z = 51
+        },
+        u_threshold = 1,
+        u_radius = 0.01
+    }}
+}
+_G.ShaderMgr = require("lib/inc/shader_inc")
+ShaderMgr:getInc():init(SHADER_RES)
